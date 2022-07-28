2246a2b2159353d94c44e5b7d18dfe0a4ed3faea
romanova-nat <natalia7@bk.ru>
2022-07-28 22:36:38 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final ProducerConsumer produceConsume = new ProducerConsumer();
+
+        final int timeUntilTheEnd = 1_000;
+
+        ThreadGroup group = new ThreadGroup("Группа");
+
+        while (produceConsume.howManyCarsBuy < 5) {
+
+            new Thread(group, () -> {
+                try {
+                    produceConsume.concume();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().isInterrupted();
+                }
+            }, "one").start();
+
+            new Thread(group, () -> {
+                try {
+                    produceConsume.concume();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().isInterrupted();
+                }
+            }, "two").start();
+
+            new Thread(group, () -> {
+                try {
+                    produceConsume.concume();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().isInterrupted();
+                }
+            }, "three").start();
+
+            new Thread(group, () -> {
+                try {
+                    produceConsume.produce();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().isInterrupted();
+                }
+            }, "Toyota").start();
+
+        }
+        Thread.sleep(timeUntilTheEnd);
+        group.interrupt();
+    }
+}
+
+
---
+++ src/main/java/ProducerConsumer.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ProducerConsumer {
+
+    private static final Lock lock = new ReentrantLock(true);
+    private final Condition conditionMake = lock.newCondition(); // ожидаемое событие - изготовление авто
+    private final Condition conditionBay = lock.newCondition(); // ожидаемое событие - покупка машины
+
+    private List<Car> carForSale = new ArrayList<>();
+    private final int LIMIT = 1;
+    private final int sleepTimeForProduce = 1_000;
+    private final int sleepTimeForConcume = 500;
+    int howManyCarsBuy = 0;
+
+    public void produce() throws InterruptedException {
+        try {
+            lock.lock();
+            while (carForSale.size() == LIMIT) {
+                conditionBay.await();
+            }
+            Thread.sleep(sleepTimeForProduce);
+            carForSale.add(new Car("Toyota"));
+            System.out.println("Производитель " + Thread.currentThread().getName() + " выпустил " + carForSale.size() + " авто");
+            conditionMake.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void concume() throws InterruptedException {
+
+        try {
+            lock.lock();
+            Thread.sleep(sleepTimeForConcume);
+
+            System.out.println("Покупатель " + Thread.currentThread().getName() + " вошел в салон");
+            if (carForSale.size() == 0) {
+                System.out.println("Машины нет");
+                conditionMake.await();
+            }
+
+                Thread.sleep(sleepTimeForConcume);
+                System.out.println("Покупатель " + Thread.currentThread().getName() + " уехал на новом авто");
+                howManyCarsBuy++;
+                carForSale.clear();
+                conditionBay.signal();
+
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+
+
